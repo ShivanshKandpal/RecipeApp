549176478defb66632290c9a519c6c2214b52b9b
Shivansh Kandpal <shivansh.kandpal.cse22@itbhu.ac.in>
2023-11-05 21:04:07 +0530

    hi

--- Ingredient/views.py
+++ Ingredient/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+def indexView(request):
+    # all_ingredients = ingredientItem.objects.all()
+    return render(request, 'index.html')
