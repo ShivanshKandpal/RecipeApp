5e6fe455c5180052e6a6c2f48f544d870bcb35ba
Shivansh Kandpal <shivansh.kandpal.cse22@itbhu.ac.in>
2023-11-06 22:01:25 +0530

    done

--- Ingredient/forms.py
+++ Ingredient/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Ingredient
 
+# class IngredientForm(forms.Form):
+#     ingredients = forms.ModelMultipleChoiceField(
+#         queryset=Ingredient.objects.all(),
+#         widget=forms.CheckboxSelectMultiple
+#     )
 class IngredientForm(forms.Form):
-    ingredients = forms.ModelMultipleChoiceField(
-        queryset=Ingredient.objects.all(),
-        widget=forms.CheckboxSelectMultiple
-    )
+    ingredient_names = forms.CharField(label='Ingredient Names', max_length=255)
